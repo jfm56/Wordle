16d15f8bd89fbbe6ddb1b9613b0d028af2de617b
Jim Mullen <jimmullen@Jims-MacBook-Pro.local>
2025-02-13 16:06:00 -0500

    In Wordle/__init__.py, added main game loop.

--- Wordle/__init__.py
+++ Wordle/__init__.py
@@ -17,3 +17,26 @@ class Wordle:
                         for g, s in zip(guess, secret_word)
     ]
         return "".join(feedback_symbols)
+
+    @staticmethod
+    def wordle():
+        """Main game loop"""
+        secret_word = Wordle.generate_word()
+        attempts = 6
+
+        print("Welcome to Wordle! You have 6 attempts to guess a five-letter word.")
+
+        for attempt in range(1, attempts + 1):
+            guess = input (f"Attempt {attempt}: Enter a five-letter word: ").lower()
+
+            if len(guess) != 5:
+                print("Invalad input. please enter exactly five letters.")
+                continue
+
+            print ("Feedback:", Wordle.check_guess(secret_word, guess))
+
+            if guess == secret_word:
+                print("Congratulations! You guessed the word corresctly.")
+                return
+
+        print (f"Game over! The correct word was: {secret_word}")
